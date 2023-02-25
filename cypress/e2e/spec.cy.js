d240348ca8c26cd79bf77d3a71c9f4553bcb04cc
szymonW <sw@fakemail.com>
2023-02-25 15:19:35 +0100

    init commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+
+describe('My First Test', () => {
+  it('Visite the HomePAge', () => {
+    cy.visit('http://qalab.pl.tivixlabs.com')
+  })
+})
